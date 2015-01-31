5d1e0aa63b9ba47a9ed66c99ea25bd8854931119
Wojtek Franke <enajski@gmail.com>
2015-01-31 14:35:48 +0100

    More bells and whistles

--- jungle.rb
+++ jungle.rb
@@ -21,39 +21,66 @@ end
 intro = [:puci, :puci, :puci, :puci, :taci, :taci, :taci, :ciu]
 main1 = [:cii,  :cii,  :taci, :taci, :cii,  :cii , :cii,  :cii]
 solo1 = main1.shuffle[0..-2] + [:ciu]
+verse1 = [:puci, :taci, :puci, :taci, :taci, :taci, :puci, :ciu]
 
-def riff
-  with_fx :echo, phase: [0.3, 0.6, 1.2].choose, mix: 0.2 do
-    use_synth synth_mix(prophet: 0, tb303: 1, noise: 0, square: 0).choose
-    play choose(chord(:c2, :minor)), release: [0.1, 0.2, 0.3].choose, amp: rrand(0.2, 2)
+def riff(prophet_ctl: 0, tb303_ctl: 0, noise_ctl: 0, square_ctl: 0)
+  with_fx :echo, phase: 0.15, mix: 0.6 do
+    puts "#{prophet_ctl} #{tb303_ctl}, #{noise_ctl}, #{square_ctl}"
+    use_synth synth_mix(prophet: prophet_ctl, tb303: tb303_ctl, noise: noise_ctl, square: square_ctl).choose
+    play choose(chord(:c3, :m7)), release: [0.1, 0.2, 0.3].choose, amp: rrand(0.2, 1.5)
     sleep map_durations(conservative: 3, wild: 1).choose
   end
 end
 
-def synth_mix(prophet: 1, tb303: 2, noise: 1, square: 5)
+def synth_mix(prophet: 0, tb303: 0, noise: 0, square: 0)
   ([:supersaw] * prophet) + ([:tb303] * tb303) + ([:pnoise] * noise) + ([:square] * square)
+end
+
+def references(amp: 0.5, rate: 1.0)
+  with_fx :echo, phase: 0.15, mix: 0.6 do
+    with_fx :hpf, cutoff: 40 do
+      with_fx :wobble, amp: amp, cutoff_min: 60, cutoff_max: 128, filter: 0 do
+        sample :bass_voxy_c, amp: rrand(0.1, 0.4), rate: rate
+      end
+    end
+    sleep map_durations(conservative: 2, wild: 1, slow: 3).choose
+  end
 end
 
 define :drums do
   with_fx :reverb, room: 0.2, mix: 0.05 do
     distort do
-      2.times { verse intro }
-      verse main1
       with_fx :ixi_techno, res: 0.4, amp: 0.8 do
+        3.times { verse intro }
         verse solo1
       end
+      4.times { verse verse1 }
     end
   end
 end
 
 define :bass do
   random_pan do
-    with_fx :rlpf, cutoff: rrand(50, 130), res: [0.1, 0.2].choose do
+    with_fx :rlpf, cutoff: rrand(20, 30), res: [0.1, 0.2].choose do
       distort do
-        3.times { riff }
+        4.times { riff(prophet_ctl: 0, tb303_ctl: 0, noise_ctl: 1, square_ctl: 0) }
+      end
+    end
+    with_fx :rlpf, cutoff: rrand(60, 130), res: [0.1, 0.2].choose do
+      distort do
+        4.times { riff(tb303_ctl: 1, square_ctl: 0) }
       end
     end
   end
+end
+
+define :samplepack do
+  references amp: 0.1, rate: 0.1
+  references amp: 0.2, rate: 0.2
+  references amp: 0.3, rate: 0.5
+  references amp: 0.4, rate: 0.8
+  4.times { references amp: 0.7, rate: 4.0 }
+
 end
 
 def distort(&block)
@@ -74,18 +101,22 @@ end
 
 def word(note)
   self.send(note)
-  self.send([:faster, :slower].choose)
+  self.send(playing_styles.choose)
+end
+
+def playing_styles
+  [:faster, :slower]
 end
 
 def faster
-  sleep map_durations(conservative: 6, wild: 3, slow: 0).choose
+  sleep map_durations(conservative: 6, wild: 4, slow: 0).choose
 end
 
 def slower
-  sleep map_durations(conservative: 6, wild: 1, slow: 1).choose
+  sleep map_durations(conservative: 6, wild: 2, slow: 1).choose
 end
 
-def map_durations(conservative: 7, wild: 1, slow: 1)
+def map_durations(conservative: 8, wild: 0, slow: 0)
   ([0.15] * wild) + ([0.3] * conservative) + ([0.45] * slow)
 end
 
@@ -98,5 +129,11 @@ end
 in_thread(name: :basser) do
   loop do
     bass
+  end
+end
+
+in_thread(name: :sampler) do
+  loop do
+    samplepack
   end
 end
